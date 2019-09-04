32b367d989785907db6699682306943423fab33f
Calvin Manosa <cmanosa@gmail.com>
2019-09-04 00:31:56 -0700

    First Commit

---
+++ Word Guess Game/assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Text: "Press any key to get started!"
+// Set a variable for Wins
+// Text: "Current Word"
+// Produce "_" Underlines for the amount letters in the word that needs to be guessed.
+// Find a way to randomize a word
+// Set a variable for Number of Guesses Remaining
+// Text: Number of Guesses Remaining
+// Text: "Letters Already Guessed"
+// Set an area that shows the already guessed letters
+// Make sure that if they repeat a letter it doesn't count towards their guesses
